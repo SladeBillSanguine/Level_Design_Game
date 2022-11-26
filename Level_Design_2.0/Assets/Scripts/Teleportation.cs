f1f889313b121285b9e10235d01c83e9899c7f76
RizeOfTheUndead <julian.philip.weber@gmail.com>
2022-11-26 19:03:36 +0100

    Teleportation funktioniert Jetzt!

--- Level_Design_2.0/Assets/Scripts/Teleportation.cs
+++ Level_Design_2.0/Assets/Scripts/Teleportation.cs
@@ -6,12 +6,12 @@ public class Teleportation : MonoBehaviour
 {
     [SerializeField] float yPosition;
     [SerializeField] Transform player;
+    [SerializeField] CharacterController playerController;
 
     public void NewPosition()
     {
-       player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yPosition, player.transform.position.z);
-       Debug.Log("Spieler Teleportiert");
-       Debug.Log(player.transform.position);
-       
+        playerController.enabled = false; 
+        player.transform.position += new Vector3(0, yPosition, 0);
+        playerController.enabled = true;
     }
 }
