4bf0f895cab801f068c4589d050f8f67fd02c338
97lici <118619931+97lici@users.noreply.github.com>
2023-01-02 13:20:39 +0100

    UX - Alicia
    Stuff for UX

---
+++ Level_Design_2.0/Assets/Alicia - UX/Monster/LookAtTarget.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public Transform target;
+
+    private void Update()
+    {
+        if(target != null)
+        {
+            transform.LookAt(target);
+        }
+    }
+
+}
