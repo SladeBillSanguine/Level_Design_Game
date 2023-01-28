6da2d418c4aef483811fee71847d58822eff895d
RizeOfTheUndead <julian.philip.weber@gmail.com>
2023-01-28 19:28:58 +0100

    Assets

---
+++ Level_Design_2.0/Assets/Allgemeine Assets/Türen/JJ_InteriorDoorPack_V1/Animation/DoorAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+
+    Animator animator; 
+    
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayDoorAnimation()
+    {
+        Debug.Log("Hello There");
+        if(animator.GetBool("isOpen") == false)
+        {
+            animator.SetBool("isOpen", true);
+        }
+        else
+        {
+            animator.SetBool("isOpen", false);
+        }
+    }
+}
