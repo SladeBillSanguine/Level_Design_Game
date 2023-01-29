46894f4ad892ce8fde2773c62cad45d77303bf6b
RizeOfTheUndead <julian.philip.weber@gmail.com>
2023-01-29 20:22:40 +0100

    LoadNextScene (Tutorial)

--- Level_Design_2.0/Assets/Scripts/LoadNextLevel.cs
+++ Level_Design_2.0/Assets/Scripts/LoadNextLevel.cs
@@ -5,20 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LoadNextLevel : MonoBehaviour
 {
-    public void LoadLevel1()
+    public void LoadTutorial()
     {
         SceneManager.LoadScene(1);
     }
-    public void LoadLevel2()
+    public void LoadLevel1()
     {
         SceneManager.LoadScene(2);
     }
-    public void LoadLevel3()
+    public void LoadLevel2()
     {
         SceneManager.LoadScene(3);
     }
-    public void LoadLevel4()
+    public void LoadLevel3()
     {
         SceneManager.LoadScene(4);
+    }
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene(5);
     }
 }
