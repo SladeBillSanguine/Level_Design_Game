67e93bd876d8a4c9291236353ad4c2a7eb7428f0
RizeOfTheUndead <julian.philip.weber@gmail.com>
2023-01-29 19:59:58 +0100

    Changeeeeeeeeees

---
+++ Level_Design_2.0/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
