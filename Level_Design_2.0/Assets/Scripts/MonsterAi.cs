c0d6c425cd4e62a79715d278d0e4c4e0f4f4ef77
Lisa <93405464+SladeBillSanguine@users.noreply.github.com>
2023-02-02 23:44:16 +0100

    fixes

--- Level_Design_2.0/Assets/Scripts/MonsterAi.cs
+++ Level_Design_2.0/Assets/Scripts/MonsterAi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MonsterAi : MonoBehaviour
+public class MonsterAI : MonoBehaviour
 {
     bool sichtbar;
     [SerializeField] GameObject Monster;
